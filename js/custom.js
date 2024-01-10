59ffb4815cd0a54568846a24c3c88f7fd69070d5
mohsinshahnawaz <mohsinkhan257@yahoo.com>
2024-01-10 22:32:26 +1100

    added file

---
+++ js/custom.js
@@ -0,0 +1,60 @@
+ jQuery("#owl-demo").owlCarousel({
+ 
+      navigation : true, // Show next and prev buttons
+      slideSpeed : 300,
+      paginationSpeed : 400,
+      singleItem:true
+ 
+    
+  });
+
+
+  var header = document.getElementById("menu-btm");
+var btns = header.getElementsByClassName("menu-item");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+};
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:1200,
+    autoplayHoverPause:true,
+    dots: false,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+});
+
+
+(function() {
+	'use strict';
+	window.addEventListener('load', function() {
+	  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+	  var forms = document.getElementsByClassName('needs-validation');
+	  // Loop over them and prevent submission
+	  var validation = Array.prototype.filter.call(forms, function(form) {
+		form.addEventListener('submit', function(event) {
+		  if (form.checkValidity() === false) {
+			event.preventDefault();
+			event.stopPropagation();
+		  }
+		  form.classList.add('was-validated');
+		}, false);
+	  });
+	}, false);
+  })();
